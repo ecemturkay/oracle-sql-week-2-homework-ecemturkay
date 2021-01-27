b249ae57163c367fd713d71ffaef0a7aae5871bb
ecemturkay <74821095+ecemturkay@users.noreply.github.com>
2021-01-27 19:41:41 +0300

    Update Ecem_Telco_Queries.sql
    Oracle'da dil ile ilgili bir problem yaşadım, Türkçe karakterler sıkıntı oluşturduğundan repositoryleri görünür hale getirirken onları düzelttim.

--- Ecem_Telco_Queries.sql
+++ Ecem_Telco_Queries.sql
@@ -12,7 +12,7 @@ WHERE CITY LIKE 'Istan%';
 
 --9 T
 SELECT cnt_value FROM contact
-WHERE  ýs_prýmary = 0 AND CNT_TYPE = 'PHONE';
+WHERE  is_primary = 0 AND CNT_TYPE = 'PHONE';
 
 --12 T
 SELECT * FROM account
@@ -24,23 +24,23 @@ WHERE STATUS = 'DEACTIVE' ;
 
 --16 T
 SELECT * FROM account
-WHERE account_closýng_date IS NOT NULL ;
+WHERE account_closing_date IS NOT NULL ;
 
 --17 T
 SELECT * FROM account
-WHERE e_býll_emaýl IS NOT NULL ;
+WHERE e_bill_email IS NOT NULL ;
 
 --19 T
 SELECT * FROM agreement
-WHERE commýtment_end_date BETWEEN '01/01/2000' AND '01/01/2005' ;
+WHERE commitment_end_date BETWEEN '01/01/2000' AND '01/01/2005' ;
 
 --21 T
-SELECT * FROM subscrýptýon
+SELECT * FROM subscription
 WHERE create_date <= '01/01/2005' AND STATUS = 'ACTIVE' ;
 
 --22 T
 SELECT * FROM agreement
-WHERE commýtment_start_date >= '31/01/2010' ; 
+WHERE commitment_start_date >= '31/01/2010' ; 
 
 --23 T
 SELECT * FROM customer
@@ -52,16 +52,16 @@ WHERE PRODUCT_TYPE LIKE '%DSL' ;
 
 --27 T 
 SELECT *  FROM customer
-WHERE NAME LIKE '%ü%' OR SURNAME LIKE '%ü%';
+WHERE NAME LIKE '%Ã¼%' OR SURNAME LIKE '%Ã¼%';
 
 --30 T 
-SELECT ýd, agreement_ýd, country_cd, full_address FROM address
+SELECT id, agreement_id, country_cd, full_address FROM address
 WHERE COUNTRY_CD = 'UK' OR COUNTRY_CD = 'AU';
 
 --32 T
 SELECT * FROM AGREEMENT
-WHERE commýtment_duratýon = '120 MONTHS' OR commýtment_duratýon = '240 MONTHS'
-ORDER BY commýtment_duratýon ASC;
+WHERE commitment_duration = '120 MONTHS' OR commitment_duration = '240 MONTHS'
+ORDER BY commitment_duration ASC;
 
 --35 T
 SELECT COUNT(*) FROM AGREEMENT
